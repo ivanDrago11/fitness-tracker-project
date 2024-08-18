fd1a2144222560dff493a7f3a47c8e67e5f328ea
ivanDrago11 <ivansaidflores@gmail.com>
2024-08-18 10:26:28 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/home" element={<h1>Home</h1>} />
+    </Routes>
+  );
+}
+
+export default App;
